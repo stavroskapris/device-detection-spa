5ebe7768b43eba05e6c51c6595edd66bba3138c6
stavrosKap <stavros.kapridakis@spitogatos.gr>
2019-11-22 16:14:22 +0200

    complete device icon, some css fixes

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="container">
+    <div class="col-md-12">
+        <div class="pnd-device">
+            <div :class="`pnd-device-icon pnd-device-icon-${device.deviceType} pnd-device-icon-${device.deviceType}-${device.operatingSystem.toLowerCase()}`"></div>
+            <div :class="`pnd-browser-icon pnd-browser-icon-${device.browser.toLowerCase()}`"></div>
+        </div>
         <p class="">{{device.browser}}</p>
         <p class="">{{device.operatingSystem}}</p>
         <p class="">{{device.deviceType}}</p>
